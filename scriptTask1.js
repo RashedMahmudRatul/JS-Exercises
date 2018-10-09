d13c2b895a4767b459e8e2358f3f077a738280db
Rashed Mahmud Ratul <rashedmahmud689@gmail.com>
2018-10-09 15:20:04 +0000

    task3,4

--- scriptTask1.js
+++ scriptTask1.js
@@ -1,27 +1,49 @@
-<!DOCTYPE html>
-<html>
-    
-    <head>
-    
-      <title>JavaScript Exercises</title>
-    
-      <!-- Insert JavaScript file -->
-    
-      <script type="text/javascript" src="script.js"></script>
-    
-    </head>
+/* JavaScript code here */
 
-    <body>
-    
-      <h3>Basic JavaScript Exercises</h3>
-    
-      <!-- Insert script -->
-    
-      <script>
-    
-        console.log("Internal Javascript");
-    
-      </script>
-    
-    </body>
-</html>
+console.log("External JavaScript");
+function sumArray(arr)
+{
+    var sum = 0;
+    arr.forEach(function(value,index)
+    {
+        sum = sum+value;
+    });
+    return sum;
+}
+console.log(sumArray([2,4,5,6,7]));
+console.log(sumArray([2,5,5,7]));
+
+function checkEmail(emailString){
+    var emailFormat= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+    var match=emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("john@Smith.com"));
+console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+   
+   function getReadingStatus(index){
+       var book=library[index];
+       return book.readingStatus;
+   }
+   console.log(getReadingStatus(1));
+   console.log(getReadingStatus(0));
+console.log(getReadingStatus(2))
